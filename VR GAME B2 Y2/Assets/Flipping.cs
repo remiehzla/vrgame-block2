8e8030b5fd67c59bbd60b7b9d130a23ab0ece184
NOAH-GameCreator <noah.gamecreator@gmail.com>
2023-01-19 15:53:29 +0100

    Pan not go brrr anymore

---
+++ VR GAME B2 Y2/Assets/Flipping.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipping : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Spatula"))
+        {
+            animator.SetTrigger("Flip");
+        }
+    }
+}
