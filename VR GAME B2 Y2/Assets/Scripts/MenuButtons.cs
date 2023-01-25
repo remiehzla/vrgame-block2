526e3dde78541a74e929349d0ec427a98a424b45
maributlikeacatsortof <mariann.pits@gmail.com>
2023-01-25 02:21:45 +0100

    Added menu scene that might work

---
+++ VR GAME B2 Y2/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
